141013490dd3b3a33739674ece091afe2c63523c
Juan González <juan@zorros.be>
2013-11-10 15:29:09 +0100

    Brand new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpsilandInfo::Application.config.secret_key_base = 'ba4fcb6826f1050f22eaa5d22c0886157e99bc1ba7a74a909a5f6ddc2eb6811f35c91439e4fe89811b18a5fcedf37fe4cd8db55d3b12822b35cee2a2a2e00053'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpsilandInfo::Application.config.session_store :cookie_store, key: '_opsiland_info_session'
