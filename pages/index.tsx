95701ddeb954066aee673418c90113e91b3d27e4
Juan González <juan.gonzalez@femtasy.com>
2022-11-07 09:23:37 +0100

    Set page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,23 @@
 import styles from "../styles/Home.module.css";
 
 import { Raleway, Roboto } from "@next/font/google";
+import Head from "next/head";
 
 const raleway = Raleway({ subsets: ["latin"], weight: "800" });
 const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
 const Home = () => (
-  <section className={styles.Home}>
-    <h1 className={raleway.className}>Opsiland</h1>
-    <footer className={roboto.className}>
-      There is really nothing to see here.
-    </footer>
-  </section>
+  <>
+    <Head>
+      <title>Opsiland</title>
+    </Head>
+    <section className={styles.Home}>
+      <h1 className={raleway.className}>Opsiland</h1>
+      <footer className={roboto.className}>
+        There is really nothing to see here.
+      </footer>
+    </section>
+  </>
 );
 
 export default Home;
